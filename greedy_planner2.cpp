61dd305286e0fa54cb66766759d98d223973fe2e
Louis Dressel <dressel@stanford.edu>
2016-05-09 21:17:23 -0700

    a greedy planner that searches over entire range

---
+++ greedy_planner2.cpp
@@ -0,0 +1,82 @@
+#include "greedy_planner2.h"
+
+using std::endl;
+
+
+GreedyPlanner2::GreedyPlanner2(string paramfile, string logpath)
+	:MyPlanner(paramfile, logpath) {}
+
+int GreedyPlanner2::initialize()
+{
+	/* Create the logging file */
+	planner_log.open(_log_path + "/planner_log.txt");
+	if (!planner_log.is_open())
+		return -1;
+
+	string path = read_config(_param_file);
+	if (path == "error")
+		return -1;
+
+	/* check that the filter is of  the correct type */
+	if (this->filter->type != 0)
+	{
+		planner_log << "Greedy2 expects a discrete filter." << endl;
+		return -1;
+	}
+	DF * f = static_cast<DF *>(this->filter);
+	_n = f->n;
+	_cell_size = f->cell_size;
+
+	/* unrecognized sensor type, this should error out */
+	return 0;
+}
+
+vector<float> GreedyPlanner2::action()
+{
+	update_belief();
+	return find_best_action();
+}
+
+
+/**
+ * Loops through all possible actions, selecting the best.
+ */
+vector<float> GreedyPlanner2::find_best_action()
+{
+	int xi, yi;
+	vector<double> xp;
+	double mi, best_mi, best_x, best_y, half_cell;
+	best_mi = -99999.0;
+	half_cell = _cell_size / 2;
+	xp.resize(3);
+	xp[2] = 0;
+
+
+	for (xi = 0; xi < _n; xi++)
+	{
+		xp[0] = xi * _cell_size + half_cell;
+		for (yi = 0; yi < _n; yi++)
+		{
+			xp[1] = yi * _cell_size + half_cell;
+			mi = filter->mutual_information(_uav, xp);
+			if (mi > best_mi)
+			{
+				best_mi = mi;
+				best_x = xp[0];
+				best_y = xp[1];
+			}
+		}
+	}
+
+	// move the vehicle too
+	double dx = best_x - _uav.x;
+	double dy = best_y - _uav.y;
+	vector<float> command;
+	command.resize(3);
+	command[0] = dy;
+	command[1] = dx;
+	command[2] = 0;
+
+	_uav.move(command);
+	return command;
+}
---
+++ greedy_planner2.h
@@ -0,0 +1,33 @@
+#ifndef GREEDY_H_
+#define GREEDY_H_
+
+#include "myplanner.h"
+
+class GreedyPlanner2 : public MyPlanner
+{
+	public:
+
+		/**
+		 * Constructor
+		 */
+		GreedyPlanner2(string paramfile, string logfile);
+
+		/**
+		 * Required function to direct vehicle.
+		 *
+		 * Returns a 3-element vector of floats: (d_north, d_east, d_yaw).
+		 */
+		vector<float> action();
+
+		/**
+		 * Returns true if an error occurred.
+		 */
+		int initialize();
+
+		// TODO: function that prints out actions so we can see if correct
+
+		vector<float> find_best_action();
+		int _n;
+		double _cell_size;
+};
+#endif
