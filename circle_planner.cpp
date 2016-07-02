113bcdc299e4b4a95a2338c054140417f2a54d8e
Louis Dressel <dressel@stanford.edu>
2016-07-02 14:26:34 -0700

    cleaning up comments for circle_planner

--- circle_planner.cpp
+++ circle_planner.cpp
@@ -29,14 +29,9 @@ int CirclePlanner::initialize()
 
 Action CirclePlanner::get_action()
 {
-	fprintf(_plannerlog, "Circle action start.\n");
-	fflush(_plannerlog);
 	// ok, we've updated belief. Now pick action
 	float ax = -cos(_bearing_max * M_PI/180.0);
 	float ay = sin(_bearing_max * M_PI/180.0);
-
-	fprintf(_plannerlog, "Circle action mid.\n");
-	fflush(_plannerlog);
 	
 	// prevent circle from switching directions
 	// if dot product is negative from last attempt, direction is different
@@ -46,28 +41,13 @@ Action CirclePlanner::get_action()
 		ay = -ay;
 	}
 
-	fprintf(_plannerlog, "Circle action mid2.\n");
-	fflush(_plannerlog);
-
-	/*
-	vector<float>commands (3);
-	commands[0] = _uav.max_step * ay;
-	commands[1] = _uav.max_step * ax;
-	commands[2] = 0.0;
-	*/
-
-	Action action{};
-	Action::set_relative_motion(&action, _uav.max_step * ay, _uav.max_step * ax);
-
-	fprintf(_plannerlog, "Circle action mid3.\n");
-	fflush(_plannerlog);
-
 	// preserve current action as last to check for direction
 	last[0] = ax;
 	last[1] = ay;
 
-	fprintf(_plannerlog, "Circle action last.\n");
-	fflush(_plannerlog);
+	// create and return action
+	Action action{};
+	Action::set_relative_motion(&action, _uav.max_step * ay, _uav.max_step * ax);
 
 	return action;
 }
